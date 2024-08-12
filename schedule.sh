085ecfd561ffd1957d12b0fb2778e09cac3d77fe
hoang@r4080 <hoang@r4080.com>
2024-08-12 18:24:13 +0900

    add schedule

--- schedule.sh
+++ schedule.sh
@@ -1,3 +1,5 @@
-python  train_tdeed.py  --model KOVO_big
-python  train_tdeed.py  --model KOVOTF_big
-python  train_tdeed.py  --model KOVOMAM_big
+mkdir -p logs/txt
+
+python  train_tdeed.py  --model KOVO_big | tee logs/txt/KOVO_big.log
+python  train_tdeed.py  --model KOVOTF_big | tee logs/txt/KOVOTF_big.log
+python  train_tdeed.py  --model KOVOMAM_big | tee logs/txt/KOVOMAM_big.log
